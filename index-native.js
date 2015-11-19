92cfd0f35802322c1aed50bf3e059cf00cbdf45e
Haadcode <haad@headbanggames.com>
2015-11-19 12:11:29 +0100

    Native app: catch errors, fix broken functionality, add min width and height for windows, add centering for windows

--- index-native.js
+++ index-native.js
@@ -12,8 +12,8 @@ var utils         = require('./core/utils');
 require('crash-reporter').start();
 logger.info("Run index-native.js");
 
-var connectWindowSize = { width: 300, height: 500, minWidth: 300, minHeight: 500 };
-var mainWindowSize    = { width: 1200, height: 800 };
+var connectWindowSize = { width: 300, height: 500, center: true, minWidth: 300, minHeight: 500 };
+var mainWindowSize    = { width: 1200, height: 800, center: true, minWidth: 1200, minHeight: 800 };
 var mainWindow = null;
 
 var template = require('./menu-native')(electronApp);
@@ -29,42 +29,46 @@ electronApp.on('window-all-closed', () => {
 });
 
 electronApp.on('ready', async(() => {
-  logger.info("Starting the system");
-  var events = await(main.start());
-  logger.info("System started");
+  try {
+    logger.info("Starting the system");
+    var events = await(main.start());
+    logger.info("System started");
 
-  Menu.setApplicationMenu(menu);
+    Menu.setApplicationMenu(menu);
 
-  events.on('connect', () => {
-    var pos  = mainWindow.getPosition();
-    var size = mainWindow.getSize();
-    var x    = (pos[0] + size[0]/2) - mainWindowSize.width/2;
-    var y    = (pos[1] + size[1]/2) - mainWindowSize.height/2;
-    mainWindow.setSize(mainWindowSize.width, mainWindowSize.height);
-    mainWindow.setPosition(x, y);
-  });
+    events.on('connect', () => {
+      var pos  = mainWindow.getPosition();
+      var size = mainWindow.getSize();
+      var x    = (pos[0] + size[0]/2) - mainWindowSize.width/2;
+      var y    = (pos[1] + size[1]/2) - mainWindowSize.height/2;
+      mainWindow.setSize(mainWindowSize.width, mainWindowSize.height);
+      mainWindow.setPosition(x, y);
+    });
 
-  events.on('disconnect', () => {
-    var pos  = mainWindow.getPosition();
-    var size = mainWindow.getSize();
-    var x    = (pos[0] + size[0]/2) - connectWindowSize.width/2;
-    var y    = (pos[1] + size[1]/2) - connectWindowSize.height/2;
-    mainWindow.setSize(connectWindowSize.width, connectWindowSize.height);
-    mainWindow.setPosition(x, y);
-  });
+    events.on('disconnect', () => {
+      var pos  = mainWindow.getPosition();
+      var size = mainWindow.getSize();
+      var x    = (pos[0] + size[0]/2) - connectWindowSize.width/2;
+      var y    = (pos[1] + size[1]/2) - connectWindowSize.height/2;
+      mainWindow.setSize(connectWindowSize.width, connectWindowSize.height);
+      mainWindow.setPosition(x, y);
+    });
 
-  mainWindow = new BrowserWindow(connectWindowSize);
-  mainWindow.setMinimumSize(connectWindowSize.minWidth, connectWindowSize.minHeight);
+    mainWindow = new BrowserWindow(connectWindowSize);
+    // mainWindow.setMinimumSize(connectWindowSize.minWidth, connectWindowSize.minHeight);
 
-  if(process.env.ENV === 'dev')
-    mainWindow.loadURL('http://localhost:8000/webpack-dev-server/');
-  else
-    mainWindow.loadURL('file://' + __dirname + '/client/dist/index.html');
+    if(process.env.ENV === 'dev')
+      mainWindow.loadUrl('http://localhost:8000/webpack-dev-server/');
+    else
+      mainWindow.loadUrl('file://' + __dirname + '/client/dist/index.html');
 
-  mainWindow.webContents.session.setDownloadPath(path.resolve(utils.getUserHome() + '/Downloads'))
-  // mainWindow.openDevTools();
+    mainWindow.webContents.session.setDownloadPath(path.resolve(utils.getUserHome() + '/Downloads'))
+    // mainWindow.openDevTools();
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    mainWindow.on('closed', () => {
+      mainWindow = null;
+    });
+  } catch(e) {
+    logger.error("Error in index-native:", e);
+  }
 }));
