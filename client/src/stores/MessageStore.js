660930cb4361610d95f3b17d89e702207fbd9daa
Haadcode <haad@headbanggames.com>
2015-12-03 13:37:49 +0100

    Flow types for UI

--- client/src/stores/MessageStore.js
+++ client/src/stores/MessageStore.js
@@ -21,10 +21,10 @@ var MessageStore = Reflux.createStore({
       this.canLoadMore = true;
       this.openChannels = {};
     },
-    getLatestMessage: function(channel) {
+    getLatestMessage: function(channel: string) {
       return this.messages[channel] && this.messages[channel].length > 0 ? this.messages[channel][0].hash : null;
     },
-    getOldestMessage: function(channel) {
+    getOldestMessage: function(channel: string) {
       return this.messages[channel] && this.messages[channel].length > 0 ? this.messages[channel][this.messages[channel].length - 1].hash : null;
     },
     onSocketConnected: function(socket) {
@@ -36,9 +36,7 @@ var MessageStore = Reflux.createStore({
         this.loadMessages(channel, null, this.getLatestMessage(channel), 10000);
       });
 
-      NetworkActions.leftChannel.listen((c) => {
-        delete this.messages[c];
-      });
+      NetworkActions.leftChannel.listen((c) => delete this.messages[c]);
     },
     onSocketDisconnected: function() {
       this.socket.removeAllListeners("messages");
@@ -48,16 +46,17 @@ var MessageStore = Reflux.createStore({
       this.messages = {};
       this.contents = {};
     },
-    onJoinedChannel: async function(channelInfo) {
+    onJoinedChannel: function(channelInfo) {
       console.log("open #" + channelInfo.name);
       if(!this.messages[channelInfo.name]) this.messages[channelInfo.name] = [];
+      // this.messages[channelInfo.name] = []; // uncomment to uncache the messages
       this.openChannels[channelInfo.name] = channelInfo.name;
       this.loadMessages(channelInfo.name, null, null, messagesBatchSize);
     },
-    onLeaveChannel: function(channel) {
+    onLeaveChannel: function(channel: string) {
       delete this.openChannels[channel];
     },
-    loadMessages: function(channel, startHash, endHash, amount) {
+    loadMessages: function(channel: string, startHash: string, endHash: string, amount: number) {
       if(!this.socket) {
         console.error("Socket not connected");
         return;
@@ -72,7 +71,7 @@ var MessageStore = Reflux.createStore({
       else
         this.socket.emit('channel.get', channel, startHash, endHash, amount, this.addMessages);
     },
-    addMessages: function(channel, newMessages) {
+    addMessages: function(channel: string, newMessages: Array) {
       if(newMessages && this.openChannels[channel]) {
         console.log("<-- messages: ", newMessages.length, newMessages);
         var merged    = this.messages[channel].concat(newMessages);
@@ -85,14 +84,14 @@ var MessageStore = Reflux.createStore({
         this.trigger(channel, this.messages[channel]);
       }
     },
-    onLoadOlderMessages: function(channel) {
+    onLoadOlderMessages: function(channel: string) {
       console.log("load more messages from #" + channel);
       if(!this.loading && this.canLoadMore) {
         this.canLoadMore = false;
         this.loadMessages(channel, this.getOldestMessage(channel), null, messagesBatchSize);
       }
     },
-    onLoadMessageContent: function(hash, callback) {
+    onLoadMessageContent: function(hash: string, callback) {
       if(!this.socket) {
         console.error("Socket not connected");
         return;
@@ -114,22 +113,22 @@ var MessageStore = Reflux.createStore({
         }
       });
     },
-    onSendMessage: function(channel, message, callback) {
+    onSendMessage: function(channel: string, text: string, callback) {
       if(!this.socket) {
         console.error("Socket not connected");
         return;
       }
 
-      console.log("--> send message:", message);
+      console.log("--> send message:", text);
       Actions.startLoading(channel);
-      this.socket.emit('message.send', channel, message, (err) => {
+      this.socket.emit('message.send', channel, text, (err) => {
         if(err) {
           console.log("Couldn't send message:", err.toString());
           Actions.raiseError(err.toString());
         }
       });
     },
-    onAddFile: function(channel, filePath) {
+    onAddFile: function(channel: string, filePath: string) {
       if(!this.socket) {
         console.error("Socket not connected");
         return;
@@ -141,20 +140,6 @@ var MessageStore = Reflux.createStore({
         if(err) {
           console.log("Couldn't add file:", err.toString());
           Actions.raiseError(err.toString());
-        }
-      });
-    },
-    // Actions listeners
-    onSetChannelOptions: function(channel, newReadPassword, newWritePassword) {
-      console.log("--> change password:", newReadPassword, newWritePassword);
-      this.socket.emit('channel.password', channel, newReadPassword, newWritePassword, (err) => {
-        console.log("--> passwords changed");
-        if(err) {
-          console.log("Couldn't set password:", err.toString());
-          Actions.raiseError(err.toString());
-        } else {
-          this.readPassword = newReadPassword;
-          NetworkActions.getChannelInfo(channel);
         }
       });
     },
