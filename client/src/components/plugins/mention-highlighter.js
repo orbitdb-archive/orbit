48fa40e180bc5ddb81d8e44bb1c956d61de6ddba
haad <haad@headbanggames.com>
2016-03-23 15:25:40 +0100

    Improve mention-highlighter

--- client/src/components/plugins/mention-highlighter.js
+++ client/src/components/plugins/mention-highlighter.js
@@ -1,27 +1,45 @@
-import React  from 'react';
+// Based on https://github.com/banyan/react-emoji
+
+import React from 'react';
 import assign from 'object-assign';
 
 let MentionHighlighter = () => {
   return {
     highlight(srcText, highlightText, options = {}) {
-      if (!srcText || !highlightText) return srcText;
+      if(typeof srcText !== 'string') return srcText;
+      if(!srcText || !highlightText) return srcText;
       if(highlightText === '' || srcText === '') return srcText;
 
-      return srcText.split(' ').map(word => {
-        let match = word.startsWith(highlightText)
-                || word.startsWith(highlightText + ":")
-                || word.startsWith("@" + highlightText)
-                || word.startsWith(highlightText + ",");
-        if (match) {
-          return React.createElement(
-            'span',
-            assign({className: options.highlightClassName}, options),
-            word + " "
-          );
+      const result = srcText.split(' ').map(word => {
+          let match = word.startsWith(highlightText)
+                  || word.startsWith(highlightText + ":")
+                  || word.startsWith("@" + highlightText)
+                  || word.startsWith(highlightText + ",");
+          if (match) {
+            return React.createElement(
+              'span',
+              assign({className: options.highlightClassName}, options),
+              word + " "
+            );
+          } else {
+            return word + " ";
+          }
+        });
+
+      let r = [];
+      let s = '';
+      result.forEach((e) => {
+        if(typeof e === 'string') {
+          s += e;
         } else {
-          return word + " ";
+          r.push(s);
+          r.push(e);
+          s = '';
         }
       });
+      r.push(s);
+
+      return r;
     }
   };
 };
