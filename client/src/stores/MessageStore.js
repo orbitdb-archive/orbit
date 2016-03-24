ea9b0f16cf82c3caa983c10191d866fb8ce80622
haad <haad@headbanggames.com>
2016-03-24 12:06:29 +0100

    Cleanup

--- client/src/stores/MessageStore.js
+++ client/src/stores/MessageStore.js
@@ -70,7 +70,6 @@ const MessageStore = Reflux.createStore({
     // console.log("MessageStore - open #" + channel);
     if(!this.messages[channel]) this.messages[channel] = [];
     this._resetLoadingState();
-    // this.loadMessages(channel, null, null, messagesBatchSize);
   },
   onLeaveChannel: function(channel: string) {
     // console.log("MessageStore - close #" + channel);
