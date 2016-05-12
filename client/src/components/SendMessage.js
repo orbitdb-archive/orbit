fc21d133646847f11adfb25bc4d58363000c5e44
masadeus <almaslac@gmail.com>
2016-05-12 12:50:55 +0200

    corrections applied

--- client/src/components/SendMessage.js
+++ client/src/components/SendMessage.js
@@ -42,73 +42,30 @@ class SendMessage extends React.Component {
   onKeyDown(event) {
     // console.log("KEYDOWN", event.type, event.which);
     if(event.which === 9) {
-      // Tab
+      // 'Tab': autocomplete user name
       event.preventDefault();
-      // TODO: autocomplete user name
-      // get value of the last typed letters
-      
-      const getLastChars = (message) => {
-        this.words = message.split(' ');
-        return this.words.pop();
-      }
-      
-      const lowerCasefy = (s) => {
-        UsersStore.users.forEach((value) => {
-          s.push(value.toLowerCase());
-        });
-      }
 
-      // console.log relevant variables
-      const control = (matches, words, tabs) => {
-        console.log(`%c all users = ${UsersStore.users}`, 'color:green; background-color:yellow');     
-        console.log(`%c matches = ${matches}`, 'color:green; background-color:yellow');     
-        console.log(`words = ${words}`);
-        console.log(`tabPressCounter = ${tabs}`);
-      }
-
-      if(this.tabPressCounter == null){
+      if(this.tabPressCounter == null) {
         this.tabPressCounter = 0;
-        let lastWord = getLastChars(this.refs.message.value).toLowerCase();
+        this.words = this.refs.message.value.split(' ');
+        let lastWord = this.words.pop().toLowerCase();
+        let usersLc = UsersStore.users.map((f) => f.toLowerCase());
+        this.matches = usersLc.map((f) => lastWord !== '' && f.startsWith(lastWord) ? f : null).filter((f) => f !== null)
 
-        // convert users to Lc for comparison
-        let usersLc = [];
-        lowerCasefy(usersLc);
-        
-        // get only indexes of the matching elements
-        let matchIndex = [];
-        const indexMatches = (value, index) => {
-          if(value.startsWith(lastWord)){
-            matchIndex.push(index);
-          }
-        } 
-        usersLc.forEach(indexMatches);
-        
-        // make array of matches from the original and non-lowerCasefyed user array
-        matchIndex.forEach((element) => {
-          this.matches.push(UsersStore.users[element]);
-        });
-      }
-      else{
+      } else {
         this.tabPressCounter += 1;
-        // when end of array is reached go back to the first elt
         this.tabPressCounter = this.tabPressCounter % this.matches.length;
         this.words.pop();
       }
-       
-      // control
-      control(this.matches, this.words, this.tabPressCounter);
 
-      // push the selected user name to array and convert back to string
-      this.words.push(this.matches[this.tabPressCounter]);
-      this.refs.message.value = this.words.join(' ');      
-      
+      if(this.matches.length > 0) {
+        this.words.push(this.matches[this.tabPressCounter]);
+        this.refs.message.value = this.words.join(' ');
+      }
     } else if(event.which === 186) {
       // ':'
       // TODO: bring up emoji preview
-    }
-    
-    // else reset variables
-    else{
+    } else {
       this.words = this.refs.message.value.split(' ');
       this.tabPressCounter = null;
       this.matches = [];
