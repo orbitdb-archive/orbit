818727e30144a88d9e172df365570c12f8067cf2
haad <haad@headbanggames.com>
2016-05-26 14:49:28 +0200

    Remove obsolete files

--- client/test/tests.bundle.js
+++
@@ -1,9 +0,0 @@
-// tests.webpack.js
-var context = require.context('./', true, /.+\.test\.jsx?$/);
-
-require('core-js/es5');
-
-context.keys().forEach(context);
-
-console.log(context)
-module.exports = context;
