a5b4ef9551cb42c85d027c81f4451606477a4f47
haad <haad@headbanggames.com>
2016-05-30 20:44:35 +0200

    Fix test

--- client/test/orbit.test.js
+++ client/test/orbit.test.js
@@ -7,6 +7,7 @@ const IPFS       = require('exports?Ipfs!ipfs/dist/index.js');
 const EventStore = require('orbit-db-eventstore');
 const Post       = require('ipfs-post');
 const Main       = require('../src/main');
+const Promise    = require('bluebird');
 
 // Mute logging
 const Logger = require('logplease');
@@ -28,34 +29,37 @@ const createBots = (prefix, amount) => {
 describe('Orbit Skynet', function() {
   this.timeout(600000);
 
-  const botCount = 2;
+  const botCount = 4;
   let bots = createBots('Orbit', botCount);
 
   before(function (done) {
-    const promises = bots.map((bot) => {
-      return new Promise((resolve, reject) => {
-        Main.start(new Date().getTime())
-        // Main.start(bot.id)
-          .then((res) => Object.assign(bot, res))
-          .then((res) => {
-            setTimeout(() => resolve(res), 1000);
-          })
-          .catch(reject);
-      });
-    });
+    // const promises = bots.map((bot) => {
+    //   return new Promise((resolve, reject) => {
+    //     setTimeout(() => {
+    //       Main.start(new Date().getTime())
+    //       // Main.start(bot.id)
+    //         .then((res) => Object.assign(bot, res))
+    //         .then(resolve)
+    //         // .then((res) => {
+    //         //   setTimeout(() => resolve(res), 1000);
+    //         // })
+    //         .catch(reject);
+    //     }, 1000);
+    //   });
+    // });
 
-    Promise.all(promises)
-      .then((res) => {
-        bots = res;
-        assert.equal(bots.length, botCount);
-        console.log("Waiting 3 seconds for the peers to connect...")
-        setTimeout(done, 3000);
-      })
-      .catch((e) => {
-        console.log(e.stack);
-        assert.equal(e, null);
-        done(e);
-      });
+    Promise.map(bots, (bot) => {
+      return Main.start(new Date().getTime()).then((res) => Object.assign(bot, res))
+    }, { concurrency: 1 }).then((res) => {
+      bots = res;
+      assert.equal(bots.length, botCount);
+      console.log("Waiting 3 seconds for the peers to connect...")
+      setTimeout(done, 3000);
+    }).catch((e) => {
+      console.log(e.stack);
+      assert.equal(e, null);
+      done(e);
+    });
   });
 
   after((done) => {
@@ -64,7 +68,7 @@ describe('Orbit Skynet', function() {
   });
 
   describe('network stress', function() {
-    it.only('creates an instance', (done) => {
+    it('creates an instance', (done) => {
       console.log();
       Promise.all(bots.map((bot) => {
         return bot.orbit.getSwarmPeers().then((peers) => {
@@ -72,10 +76,12 @@ describe('Orbit Skynet', function() {
           peers.forEach((e) => console.log(e));
           console.log();
           const botIds = bots.map((e) => e.peerId.ID).filter((e) => e !== bot.peerId.ID);
-          assert.equal(peers.length, 2);
+          assert.equal(peers.length, bots.length - 1);
           peers = peers.map((e) => e.split("/").pop());
-          assert.equal(peers[0] === botIds[0] || peers[0] === botIds[1], true);
-          assert.equal(peers[1] === botIds[0] || peers[1] === botIds[1], true);
+          peers.forEach((peer, index) => {
+            assert.equal(botIds.indexOf(peer) > -1, true);
+          });
+          // assert.equal(peers[1] === botIds[0] || peers[1] === botIds[1], true);
         });
       }))
       .then((res) => done())
@@ -96,7 +102,7 @@ describe('Orbit Skynet', function() {
         bot.orbit.events.on('sync', (channelName) => {
           console.log(bot.name + " started syncing", channelName);
         });
-        if(index === 1) {
+        if(index !== 0) {
           bot.orbit.events.on('message', (channelName, hash) => {
             console.log(bot.name + " received new head from pubsub", hash);
           });
@@ -107,6 +113,14 @@ describe('Orbit Skynet', function() {
         });
         return bot.orbit.connect(network, bot.name, '')
           .then(() => bot.orbit.join(channel))
+          .then(() => {
+            bot.orbit.getSwarmPeers().then((peers) => {
+              console.log();
+              console.log(`${bot.name}'s peers:`);
+              peers.forEach((e) => console.log(e));
+              console.log();
+            });
+          })
           .then(() => {
             return bot;
           });
@@ -131,9 +145,11 @@ describe('Orbit Skynet', function() {
 
           // Check the results
           setInterval(() => {
-            if(allMessagesCount === doneCount && index === 1) {
+            if(allMessagesCount === doneCount && index !== 1) {
               setInterval(() => {
+                console.log("----------------------------111")
                 const messages = bot.orbit.getMessages(channel, null, null, 100);
+                console.log("----------------------------222")
                 // console.log(bot.name + ">" + JSON.stringify(messages, null, 2));
                 assert.equal(messages.length, doneCount);
                 assert.equal(messages[0].payload.op, 'ADD');
