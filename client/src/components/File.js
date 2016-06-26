bba27c1daad55a13e882f5d8607eb09f88676f5a
Claus Wahlers <claus@codeazur.com.br>
2016-06-26 13:18:48 -0300

    Make preview for text files work

--- client/src/components/File.js
+++ client/src/components/File.js
@@ -13,6 +13,34 @@ import 'styles/File.scss';
 import Logger from 'logplease';
 const logger = Logger.create('Clipboard', { color: Logger.Colors.Magenta });
 
+function readUTF8String(bytes) {
+  let i = 0;
+  let string = '';
+  if (bytes.slice(0, 3) == '\xEF\xBB\xBF') {
+    i = 3;
+  }
+  for (; i < bytes.byteLength; i++) {
+    const byte1 = bytes[i];
+    if (byte1 < 0x80) {
+      string += String.fromCharCode(byte1);
+    } else if (byte1 >= 0xc2 && byte1 < 0xe0) {
+      const byte2 = bytes[++i];
+      string += String.fromCharCode(((byte1 & 0x1f) << 6) + (byte2 & 0x3f));
+    } else if (byte1 >= 0xe0 && byte1 < 0xf0) {
+      const byte2 = bytes[++i];
+      const byte3 = bytes[++i];
+      string += String.fromCharCode(((byte1 & 0xff) << 12) + ((byte2 & 0x3f) << 6) + (byte3 & 0x3f));
+    } else if (byte1 >= 0xf0 && byte1 < 0xf5) {
+      const byte2 = bytes[++i];
+      const byte3 = bytes[++i];
+      const byte4 = bytes[++i];
+      const codepoint = (((byte1 & 0x07) << 18) + ((byte2 & 0x3f) << 12) + ((byte3 & 0x3f) << 6) + (byte4 & 0x3f)) - 0x10000;
+      string += String.fromCharCode((codepoint >> 10) + 0xd800, (codepoint & 0x3ff) + 0xdc00);
+    }
+  }
+  return string;
+}
+
 class File extends React.Component {
 
   constructor(props) {
@@ -62,9 +90,19 @@ class File extends React.Component {
               previewContent = <img height={200} src={url} />;
             } else if (this.isVideo) {
               previewContent = <video height={200} src={url} controls autoPlay={false} />;;
-            // } else if (this.isHighlightable) {
-            //   previewContent = <Highlight>{blob}</Highlight>;
-            // } else {
+            } else {
+              var fileReader = new FileReader();
+              fileReader.onload = (event) => {
+                const text = readUTF8String(new Uint8Array(event.target.result));
+                if (this.isHighlightable) {
+                  previewContent = <Highlight>{text}</Highlight>;
+                } else {
+                  previewContent = text;
+                }
+                this.setState({ previewContent });
+              };
+              fileReader.readAsArrayBuffer(blob);
+              return;
             }
           }
           this.setState({ previewContent });
