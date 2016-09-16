b85e36e555805f936f6a18b545af941fce30f5df
haad <haad@headbanggames.com>
2016-09-16 16:34:00 +0800

    Improve login window

--- client/src/components/BackgroundAnimation.js
+++ client/src/components/BackgroundAnimation.js
@@ -64,11 +64,12 @@ class BackgroundAnimation extends React.Component {
 
     let styleSheet = document.styleSheets[0]
     var dots = rings.map((i) => {
-      const c = Math.max(255 - (i * 24), 0)
+      const speed  = 0.5
+      const velocity = (i + 1) * 9.80665 / speed
+      const c = Math.max(196 - (i * 24), 0)
       const color = `rgba(${c}, ${c}, ${c}, ${0.5 - ((i + 1) * 0.025)})`
       const mul   = (Math.random() < 0.5 ? -1 : 1) // randomize between negative and positive pos
       const pos   = (minSize + (i * inc)) * mul // starting position for the dot
-      const speed = (i + 1) * 9.80665
       const size  = (Math.random() * 2) + 1
       const startRadians = Math.floor(Math.random() * 360)
       let keyframes = `@keyframes rot${i} {
@@ -85,7 +86,7 @@ class BackgroundAnimation extends React.Component {
           fill={color}
           style={{
             animation: `rot${i} linear`,
-            animationDuration: `${speed}s`,
+            animationDuration: `${velocity}s`,
             animationIterationCount: "infinite"
           }}
           key={"dot" + i}>
@@ -95,8 +96,10 @@ class BackgroundAnimation extends React.Component {
 
     return (
       <div className="BackgroundAnimation" ref="container" style={this.props.style}>
-        <svg width={this.state.width} height={this.state.width} key="circles" style={this.state.theme} className="transparent">
-          {circles}
+        <svg width={this.state.width} height={this.state.width} key="circles" style={this.state.theme}>
+          <g className="transparent">
+            {circles}
+          </g>
           <svg width={this.state.width} height={this.state.width} key="dots" style={this.state.theme} className="opaque">
             {dots}
           </svg>
