7bed8dd08d12a0a63b6bcc55a58c37604586da90
haad <haad@headbanggames.com>
2016-12-09 15:04:13 +0100

    Add note about local signal server setup

--- client/src/config/ipfs-daemon.config.js
+++ client/src/config/ipfs-daemon.config.js
@@ -7,6 +7,8 @@ export function defaultIpfsDaemonSettings(ipfsDataDir) {
       Swarm: ['/ip4/0.0.0.0/tcp/0'],
       Gateway: '/ip4/0.0.0.0/tcp/0'
     },
+    // Use local webrtc-star server: https://github.com/libp2p/js-libp2p-webrtc-star
+    // SignalServer: '0.0.0.0:9090',
     SignalServer: '178.62.241.75',
     API: {
       HTTPHeaders: {
